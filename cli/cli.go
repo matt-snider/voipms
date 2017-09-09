3b44d44f9018ce62dbac7b5b3eb6f81ee4c9bdc9
Matt Snider <matt.snider@alum.utoronto.ca>
2017-09-10 01:51:00 +0200

    Use subcommands and default 'voipms sms' to default to fetch

--- cli/cli.go
+++ cli/cli.go
@@ -13,33 +13,47 @@ func main() {
 	password := os.Getenv("VOIPMS_PASSWORD")
 	client := api.NewClient(username, password)
 
-	// CLI App
+	/**
+	 * Command Implementations
+	 */
+	fetchSms := func(c *cli.Context) error {
+		resp, err := client.GetSms()
+		if err != nil {
+			return cli.NewExitError(err, -1)
+		}
+
+		lineFmt := "%-9s %-20s %-8s %-10s %s\n"
+		fmt.Printf(lineFmt, "ID", "Date", "Action", "Contact", "Message")
+		for _, sms := range resp.SmsList {
+			var action string
+			if sms.SmsType == "0" {
+				action = "sent"
+			} else {
+				action = "received"
+			}
+
+			fmt.Printf(lineFmt, sms.ID, sms.Date, action, sms.Contact, sms.Message)
+		}
+		return nil
+	}
+
+	/**
+	 * CLI Declaration
+	 */
 	app := cli.NewApp()
 	app.Name = "voipms"
 	app.Commands = []cli.Command{
 		{
 			Name:  "sms",
-			Usage: "Get SMS's",
-			Action: func(c *cli.Context) error {
-				resp, err := client.GetSms()
-				if err != nil {
-					return cli.NewExitError(err, -1)
-				}
-
-				lineFmt := "%-9s %-20s %-8s %-10s %s\n"
-				fmt.Printf(lineFmt, "ID", "Date", "Action", "Contact", "Message")
-				for _, sms := range resp.SmsList {
-					var action string
-					if sms.SmsType == "0" {
-						action = "sent"
-					} else {
-						action = "received"
-					}
-
-					fmt.Printf(lineFmt, sms.ID, sms.Date, action, sms.Contact, sms.Message)
-				}
-				return nil
+			Usage: "Manage SMS messages",
+			Subcommands: []cli.Command{
+				{
+					Name:   "fetch",
+					Action: fetchSms,
+				},
 			},
+			// Default to fetch
+			Action: fetchSms,
 		},
 
 		{
@@ -55,5 +69,6 @@ func main() {
 			},
 		},
 	}
+
 	app.Run(os.Args)
 }
