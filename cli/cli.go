3a1e834dc7dff4593864176d83d91ba0856bf775
Matt Snider <matt.snider@alum.utoronto.ca>
2017-09-10 01:02:22 +0200

    Use urfave/cli and implement getsms CLI cmd

--- cli/cli.go
+++ cli/cli.go
@@ -5,17 +5,32 @@ import (
 	"os"
 
 	"github.com/matt-snider/voipms/api"
+	"github.com/urfave/cli"
 )
 
 func main() {
 	username := os.Getenv("VOIPMS_USERNAME")
 	password := os.Getenv("VOIPMS_PASSWORD")
 	client := api.NewClient(username, password)
-	resp, err := client.GetSms()
-	if err != nil {
-		fmt.Println("Error - ", err)
-		os.Exit(-1)
-	} else {
-		fmt.Println("Response - ", resp)
+
+	// CLI App
+	app := cli.NewApp()
+	app.Name = "voipms"
+	app.Commands = []cli.Command{
+		{
+			Name:  "getsms",
+			Usage: "Get SMS's",
+			Action: func(c *cli.Context) error {
+				resp, err := client.GetSms()
+				if err != nil {
+					fmt.Println("Error - ", err)
+					os.Exit(-1)
+				}
+
+				fmt.Println("Resp %s", resp)
+				return nil
+			},
+		},
 	}
+	app.Run(os.Args)
 }
