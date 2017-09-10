e3dbc27a98916d2fffcab1040300095266bf70b6
Matt Snider <matt.snider@alum.utoronto.ca>
2017-09-10 22:07:06 +0200

    Form data should be multipart/form-data and not x-www-form-urlencoded

--- api/client.go
+++ api/client.go
@@ -1,8 +1,10 @@
 package api
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"mime/multipart"
 	"net/http"
 	"net/url"
 )
@@ -36,49 +38,64 @@ func NewClient(username, password string) *VoipMsClient {
 	}
 }
 
-func (c *VoipMsClient) newRequest(httpMethod, apiMethod string, params *map[string]string) (*http.Request, error) {
-	req, err := http.NewRequest(httpMethod, c.apiUrl, nil)
+func (c *VoipMsClient) do(httpMethod, apiMethod string, params map[string]string) (*http.Response, error) {
+	req, err := c.newRequest(httpMethod, apiMethod, params)
 	if err != nil {
 		return nil, err
 	}
 
-	// Build request depending on httpMethod
-	form := url.Values{}
-	if params != nil {
-		for key, value := range *params {
-			form.Add(key, value)
-		}
-	}
-	form.Add("api_username", c.username)
-	form.Add("api_password", c.password)
-	form.Add("method", apiMethod)
-	if httpMethod == http.MethodGet {
-		req.URL.RawQuery = form.Encode()
-	} else if httpMethod == http.MethodPost {
-		req.PostForm = form
-	} else {
-		return nil, fmt.Errorf("Invalid http method for voipms api: %s", httpMethod)
-	}
-	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-	req.Header.Set("Accept", "application/json")
-	req.Header.Set("User-Agent", userAgent)
-	return req, nil
-}
-
-func (c *VoipMsClient) do(request *http.Request) (*http.Response, error) {
-	resp, err := c.client.Do(request)
+	resp, err := c.client.Do(req)
 	if err != nil {
 		return nil, err
 	}
 	return resp, nil
 }
 
-func (c *VoipMsClient) GetIp() (*IPResponse, error) {
-	req, err := c.newRequest("GET", "getIP", nil)
+func (c *VoipMsClient) newRequest(httpMethod, apiMethod string, params map[string]string) (*http.Request, error) {
+	if params == nil {
+		params = make(map[string]string)
+	}
+	params["api_username"] = c.username
+	params["api_password"] = c.password
+	params["method"] = apiMethod
+
+	var url string
+	var contentType string
+	var body = &bytes.Buffer{}
+	if httpMethod == http.MethodGet {
+		url = c.apiUrl + "?" + toUrlValues(params).Encode()
+	} else if httpMethod == http.MethodPost {
+		url = c.apiUrl
+		writer := multipart.NewWriter(body)
+		for k, v := range params {
+			writer.WriteField(k, v)
+		}
+		contentType = writer.FormDataContentType()
+		writer.Close()
+	} else {
+		return nil, fmt.Errorf("Invalid http method for voipms api: %s", httpMethod)
+	}
+
+	req, err := http.NewRequest(httpMethod, url, body)
 	if err != nil {
 		return nil, err
 	}
-	resp, err := c.do(req)
+	req.Header.Set("Content-Type", contentType)
+	req.Header.Set("Accept", "application/json")
+	req.Header.Set("User-Agent", userAgent)
+	return req, nil
+}
+
+func toUrlValues(params map[string]string) url.Values {
+	values := url.Values{}
+	for k, v := range params {
+		values.Set(k, v)
+	}
+	return values
+}
+
+func (c *VoipMsClient) GetIp() (*IPResponse, error) {
+	resp, err := c.do("GET", "getIP", nil)
 	if err != nil {
 		return nil, err
 	}
@@ -110,11 +127,7 @@ type SmsResponse struct {
 }
 
 func (c *VoipMsClient) GetSms() (*SmsResponse, error) {
-	req, err := c.newRequest("GET", "getSMS", nil)
-	if err != nil {
-		return nil, err
-	}
-	resp, err := c.do(req)
+	resp, err := c.do("GET", "getSMS", nil)
 	if err != nil {
 		return nil, err
 	}
@@ -132,17 +145,15 @@ func (c *VoipMsClient) GetSms() (*SmsResponse, error) {
 }
 
 func (c *VoipMsClient) SendSms(did, dest, msg string) error {
-	params := &map[string]string{
-		"did":     url.QueryEscape(did),
-		"dst":     url.QueryEscape(dest),
-		"message": url.QueryEscape(msg),
-	}
-	req, err := c.newRequest("POST", "sendSMS", params)
-	fmt.Println(req.URL.RawQuery)
-	if err != nil {
-		return err
-	}
-	resp, err := c.do(req)
+	resp, err := c.do(
+		"POST",
+		"sendSMS",
+		map[string]string{
+			"did":     url.QueryEscape(did),
+			"dst":     url.QueryEscape(dest),
+			"message": url.QueryEscape(msg),
+		},
+	)
 	if err != nil {
 		return err
 	}
@@ -153,6 +164,7 @@ func (c *VoipMsClient) SendSms(did, dest, msg string) error {
 	if err := dec.Decode(&data); err != nil {
 		return err
 	}
+	fmt.Println(data)
 	if err := toError(data.Status); err != nil {
 		return err
 	}
@@ -171,6 +183,8 @@ func toError(status string) error {
 		detail = "This is not a valid Method"
 	case "invalid_dst":
 		detail = "This is not a valid Destination Number"
+	case "invalid_did":
+		detail = "This is not a valid DID"
 	case "missing_method":
 		detail = "Method must be provided when using the REST/JSON API"
 	case "missing_credentials":
